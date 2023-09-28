977c266b3b7eff46cf50441b0868ac1770bd6dda
abhijeets1 <abhishahakar@gmail.com>
2023-09-29 01:26:40 +0530

    Changes for Render

--- PDFEngine/urls.py
+++ PDFEngine/urls.py
@@ -34,6 +34,3 @@ urlpatterns = [
 		path('imgtotext/', include('imgtotext.urls')),
 		path('pdfcprotect/', include('pdfcprotect.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
